c409e1e44852cc0adbd88bece069b0309ed0fbaa
waksana <waksana@qq.com>
2016-12-07 15:14:51 +0800

    stream into cmd

--- index.js
+++ index.js
@@ -23,38 +23,57 @@ const replace = (str, obj) =>
 
 const wrapArr = val => (val instanceof Array) ? val: [val];
 
+const task = deploy => {
+  const tmpdir = `${os.homedir()}/.deploy/${deploy.name}`;
+  const deployPath = path.resolve(deploy.path, deploy.name);
+  const before = replace(config.before || '', deploy);
+  const after = replace(config.after || '', deploy);
+
+  if(!fs.existsSync(tmpdir)) {
+    sh(`mkdir -p ${tmpdir}`);
+    sh(`git archive --format=tar ${deploy.branch} | tar x -C ${tmpdir}`);
+    if(before.trim() != '') sh(before, {cwd: tmpdir});
+    deploy.ssh.forEach(ssh => {
+      sh(ssh(`rm -rf ${deployPath}`));
+      sh(ssh(`mkdir -p ${deployPath}`));
+      sh(`tar cC ${tmpdir} . | ` + ssh(`tar xC ${deployPath}`));
+      if(after.trim() != '')
+        sh(ssh(''), {input: `cd ${deployPath} && ${after}`})
+      sh(`rm -rf ${tmpdir}`);
+    });
+  }
+  else {
+    sh(`echo ${tmpdir} existed`);
+  }
+}
+
 const deploy = config.deploy[env];
 
 if(deploy) {
+  deploy.env = env;
+  deploy.name = `${config.name}_${env}`;
   deploy.ssh = wrapArr(deploy.ssh).map(ssht);
+
   if(run.length > 0) {
     let cmd = run.join(' ');
     deploy.ssh.map(ssh => sh(ssh(''), {input: cmd}));
   }
   else {
-    deploy.env = env;
-    deploy.name = `${config.name}_${env}`;
-    const tmpdir = `${os.homedir()}/.deploy/${deploy.name}`;
-    const deployPath = path.resolve(deploy.path, deploy.name);
-    const before = replace(config.before || '', deploy);
-    const after = replace(config.after || '', deploy);
-
-    if(!fs.existsSync(tmpdir)) {
-      sh(`mkdir -p ${tmpdir}`);
-      sh(`git archive --format=tar ${deploy.branch} | tar x -C ${tmpdir}`);
-      if(before.trim() != '') sh(before, {cwd: tmpdir});
-      deploy.ssh.forEach(ssh => {
-        sh(ssh(`rm -rf ${deployPath}`));
-        sh(ssh(`mkdir -p ${deployPath}`));
-        sh(`tar cC ${tmpdir} . | ` + ssh(`tar xC ${deployPath}`));
-        if(after.trim() != '')
-          sh(ssh(''), {input: `cd ${deployPath} && ${after}`})
-        sh(`rm -rf ${tmpdir}`);
-      });
-    }
-    else {
-      sh(`echo ${tmpdir} existed`);
-    }
+    process.stdin.once('readable', () => {
+      const chunk = process.stdin.read();
+      if(chunk) {
+        var bufs = [chunk];
+        process.stdin.on('data', bufs.push.bind(bufs));
+        process.stdin.on('end', () => {
+          const input = Buffer.concat(bufs);
+          deploy.ssh.map(ssh => sh(ssh(''), {input}));
+        });
+      }
+      else {
+        task(deploy);
+        process.exit();
+      }
+    });
   }
 }
 else {
